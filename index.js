cb07c04a9db44c2fe91fec9c75fc59f5f202dc9e
Oksana Smolynets <oksana.mavdryk25@gmail.com>
2020-01-20 14:14:54 +0200

    share button

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function Welcome() {
-    // document.getElementById("welcome").style.background = "green";
-    document.getElementById("welcome").style.display = "none";
+    document.getElementById("welcome").style.background = "rgb(70, 53, 42)";
+    document.getElementById("welcome").innerHTML = "Good luck!";
+    document.getElementById("welcome").style.color = "white";
+    document.getElementById("welcome").style.borderColor = "white";
+    document.getElementById("welcome").style.boxShadow = "none";
 }
